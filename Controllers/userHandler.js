bc4e022897f506ebe47027ea23b794a753610fb1
Govinda <upadhyagovinda52@gmail.com>
2025-09-05 11:23:05 +0600

    done

--- Controllers/userHandler.js
+++ Controllers/userHandler.js
@@ -1,24 +1,16 @@
 import { base_url } from "../index.js";
 import fs from "fs/promises";
-export const adminUpload = async (req, res) => {
+export const userUpload = async (req, res) => {
   if (!req.file) return res.status(400).json({ message: "No file uploaded" });
   const relativePath = req.file.path.replace("uploads/", "");
   const fileUrl = `${base_url}/photos/${relativePath}`;
   res.json({ message: "File uploaded successfully!", url: fileUrl });
 };
-export const adminUploads = async (req, res) => {
-  if (!req.files || req.files.length === 0) {
-    return res.status(400).json({ message: "No files uploaded" });
-  }
-  const fileUrls = req.files.map((file) => {
-    const relativePath = file.path.replace("uploads/", "");
-    return `${base_url}/photos/${relativePath}`;
-  });
-  res.json({ message: "Files uploaded successfully!", urls: fileUrls });
-};
-export const adminUploadDelete = async (req, res) => {
+
+export const userUploadDelete = async (req, res) => {
   const { url } = req.body;
   const path = url.split("/").splice(4, 2);
+
   const main = `uploads/${path[0]}/${path[1]}`;
 
   try {
--- index.js
+++ index.js
@@ -2,16 +2,24 @@ import e from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import adminUploadRoutes from "./Routes/admin.js";
+import { userUpload } from "./Controllers/userHandler.js";
+import { userUploadRouter } from "./Routes/user.js";
 
 const app = e();
-export const base_url = "http://localhost:3002";
+export const base_url = "https://www.thanggo.com";
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["https://www.thanggo.com", "https://thanggo.com"],
+    credentials: true,
+  })
+);
 app.use(e.json({ limit: "50mb" }));
 app.use("/photos", e.static("uploads"));
 app.use(e.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
+app.use("/user", userUploadRouter);
 app.use("/admin", adminUploadRoutes);
 
 app.listen(3002, () => console.log("listening.."));
