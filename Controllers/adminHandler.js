086c7a49d83655b6c07413fd1f21b45e070b340d
Govinda <upadhyagovinda52@gmail.com>
2025-09-05 20:21:37 +0600

    doing

--- Controllers/adminHandler.js
+++ Controllers/adminHandler.js
@@ -12,7 +12,7 @@ export const adminUploads = async (req, res) => {
   }
   const fileUrls = req.files.map((file) => {
     const relativePath = file.path.replace("uploads/", "");
-    return `${base_url}/photos/${relativePath}`;
+    return `${base_url}/api/photo/photos/${relativePath}`;
   });
   res.json({ message: "Files uploaded successfully!", urls: fileUrls });
 };
@@ -20,10 +20,7 @@ export const handleSignupUpload = async (req, res) => {
   if (!req.file) {
     return res.status(400).json({ message: "No file uploaded" });
   }
-  console.log("req.file:", req.file);
-  console.log("req.body:", req.body);
 
-  // Ensure path exists and normalize slashes
   const filePath = req.file.path;
   const relativePath = filePath.replace("uploads/", "");
   const fileUrl = `${base_url}/api/photo/photos/${relativePath}`;
