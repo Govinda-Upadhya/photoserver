41969e40ffec04b4c25d0b3e61e823648d763673
Govinda <upadhyagovinda52@gmail.com>
2025-09-06 14:39:18 +0600

    done

--- Controllers/adminHandler.js
+++ Controllers/adminHandler.js
@@ -50,6 +50,6 @@ export const adminUploadDelete = async (req, res) => {
     return res.status(200).json({ msg: "deleted successfully" });
   } catch (err) {
     console.error("Error deleting file:", err);
-    return res.status(400).json({ msg: "couldnt delete the image" });
+    return res.status(400).json({ msg: "couldnt delete the image", err });
   }
 };
